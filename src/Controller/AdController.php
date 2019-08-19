9cec964711fba04039ded91e5cf8fa391d3906b7
Hiorel <s.leclerc076@outlook.fr>
2019-08-19 10:46:17 +0200

    maj

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Ad;
 use App\Form\AdType;
+use App\Entity\Image;
 use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -35,6 +36,19 @@ class AdController extends AbstractController
      */
     public function create(Request $request, ObjectManager $manager){
         $ad = new Ad();
+
+        $image = new Image();
+
+        $image->setUrl('http://placehold.it/400x200')
+              ->setCaption('Titre 1');
+
+        $image2 = new Image();
+
+        $image2->setUrl('http://placehold.it/400x200')
+              ->setCaption('Titre 2');
+
+        $ad->addImage($image);
+        $ad->addImage($image2);
 
         $form = $this->createForm(AdType::class, $ad);
 
