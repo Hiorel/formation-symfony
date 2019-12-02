94cf564fa4d2c2144d5d4d1b33d01145be71c8b3
Hiorel <s.leclerc076@outlook.fr>
2019-12-02 17:01:16 +0100

    Ajout de la logique des roles et de la securité

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -10,7 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -31,6 +33,8 @@ class AdController extends AbstractController
      * Permet de créer une annonce
      * 
      * @Route("/ads/new", name="ads_create")
+     * 
+     * @IsGranted("ROLE_USER")
      *
      * @return Response
      */
@@ -71,6 +75,8 @@ class AdController extends AbstractController
      * Permet d'afficher le formulaire dédition
      * 
      * @Route("/ads/{slug}/edit", name="ads_edit")
+     * 
+     * @Security("is_granted('ROLE_USER') and user === ad.getAuthor()", message="Cette annonce ne vous appartient pas, vous ne pouvez pas la modifier")
      *
      * @return Response
      */
@@ -115,5 +121,27 @@ class AdController extends AbstractController
         return $this->render('ad/show.html.twig', [
             'ad' => $ad
         ]);
+    }
+    /**
+     * Permet de supprimer une annonce
+     *
+     * @Route("/ads/{slug}/delete", name="ads_delete")
+     * 
+     * @Security("is_granted('ROLE_USER') and  user == ad.getAuthor()", message ="Vous n'avez pas le droit d'accéder à cette ressource")
+     * 
+     * @param Ad $ad
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function delete(Ad $ad, ObjectManager $manager){
+        $manager->remove($ad);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "L'annonce <strong>{$ad->getTitle()}</strong> a bien été supprimée !"
+        );
+
+        return $this->redirectToRoute("ads_index");
     }
 }
