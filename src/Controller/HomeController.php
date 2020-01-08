7285c507845fe5d3c9a68d3ab85228e0c27e1fba
Hiorel <s.leclerc076@outlook.fr>
2020-01-08 09:43:34 +0100

    Mise en place de Encore CSS JS

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,36 +11,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController {
 
     /**
-     * @Route("/bonjour/{prenom}/age/{age}", name="hello")
-     * @Route("/salut", name="hello_base")
-     * @Route("/bonjour/{prenom}", name="hello_prenom")
-     * 
-     * Montre la page qui dit bonjour
-     * 
-     * @return void
-     */
-    public function hello($prenom = "anonyme", $age = "0"){
-      return $this->render(
-          'hello.html.twig',
-          [
-              'prenom' => $prenom,
-              'age' => $age
-          ]
-      );      
-    }
-
-    /**
      * @Route("/", name="homepage")
      */
-    public function home(){
-        $prenoms = ["Lior" => 31,"Joseph" => 16,"Anne" => 56];
-
+    public function home(AdRepository $adRepo, UserRepository $userRepo){
+      
         return $this->render(
             'home.html.twig',
             [
-                'title' => "Bonjour à tous",
-                'age' => 15,
-                'tableau' => $prenoms
+                'ads' => $adRepo->findBestAds(3),
+                'users' => $userRepo->findBestUsers(2)
             ]
         );
     }
